34a14ae74f8773a6e191fbe36535f7c242d9d713
DJBlack Phoénix <pasmax2@gmail.com>
2025-03-17 18:52:30 -0400

    Ajout du copyright et de quelle que information en lien avec le projet.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Codage By DJBlack copyright 2024/2025
+// Discord: djblack.
+// Github: https://github.com/pasmax2/punch
 const { Client, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
 require('dotenv').config();
