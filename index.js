b46db2e5fa5d5e345c4021047456cf37689cecbe
DJBlack Phoénix <pasmax2@gmail.com>
2025-03-22 14:18:40 -0400

    update index.js deleted shutdown.js

--- index.js
+++ index.js
@@ -13,7 +13,18 @@ const client = new Client({
     ]
 });
 
-require('./shutdown'); // Importer le module de gestion de l'extinction du bot
+process.on('SIGINT', () => {
+    console.log('Détection de fermeture du processus (SIGINT), déconnexion du bot...');
+    if (client) client.destroy();
+    process.exit(0);
+});
+
+process.on('SIGTERM', () => {
+    console.log('Détection de fermeture du processus (SIGTERM), déconnexion du bot...');
+    if (client) client.destroy();
+    process.exit(0);
+});
+
 
 // Stocke l'ID du propriétaire du bot dans une variable d'environnement
 const botOwnerId = process.env.BOT_OWNER_ID; 
